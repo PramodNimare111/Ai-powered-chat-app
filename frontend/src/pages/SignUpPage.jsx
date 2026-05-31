41fb94c85e28ba36b89fa360ce3a961fac60f235
pramod nimare <modelpramod2002@gmail.com>
2026-05-31 14:15:56 +0530

    updating UI

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -15,7 +15,7 @@ function SignUpPage() {
 
   return (
     <div className="w-full flex items-center justify-center p-4 bg-slate-900">
-      <div className="relative w-full max-w-6xl md:h-[800px] h-[650px]">
+      <div className="relative w-full max-w-6xl md:h-[730px] h-[650px]">
         <BorderAnimatedContainer>
           <div className="w-full flex flex-col md:flex-row">
             {/* FORM CLOUMN - LEFT SIDE */}
@@ -23,9 +23,21 @@ function SignUpPage() {
               <div className="w-full max-w-md">
                 {/* HEADING TEXT */}
                 <div className="text-center mb-8">
-                  <MessageCircleIcon className="w-12 h-12 mx-auto text-slate-400 mb-4" />
-                  <h2 className="text-2xl font-bold text-slate-200 mb-2">Create Account</h2>
-                  <p className="text-slate-400">Sign up for a new account</p>
+                  <div className="text-center mb-8">
+                    <img
+                      src="/logo.png"
+                      alt="Logo"
+                      className="w-36 h-36 mx-auto mb-4 object-contain drop-shadow-[0_0_20px_rgba(34,211,238,0.5)]"
+                    />
+
+                    <h2 className="text-3xl font-bold text-slate-200">
+                      Create Account
+                    </h2>
+
+                    <p className="text-slate-400 mt-2">
+                      Sign up for a new account
+                    </p>
+                  </div>
                 </div>
 
                 {/* FORM */}
